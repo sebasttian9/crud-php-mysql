b5c725c3c73f311a43ae6fd69e0fd5f745f2ddd2
Sebastian Valenzuela <svalenzuela.ortiz@gmail.com>
2023-03-23 10:17:32 -0300

    deploy en heroku

--- SQL/config.php
+++ SQL/config.php
@@ -2,10 +2,10 @@
 
 return [
   'db' => [
-    'host' => 'localhost',
-    'user' => 'root',
-    'pass' => '',
-    'name' => 'desafio_crud',
+    'host' => 'us-cdbr-east-06.cleardb.net',
+    'user' => 'b1f6e0bee8bfd8',
+    'pass' => '7ab25f50',
+    'name' => 'heroku_0c55747f18e662f',
     'options' => [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]
